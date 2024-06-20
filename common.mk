3bd569b33e0ae4da7ccd446d9cab04d75eeca92c
Joey <joey@evolution-x.org>
2024-06-21 02:45:19 +0900

    sm8450-common: Declare more media packages for Dolby

--- common.mk
+++ common.mk
@@ -299,9 +299,16 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.2.vendor \
     libOmxCore \
+    libavservices_minijail.vendor \
+    libavservices_minijail_vendor \
+    libcodec2_hidl@1.2.vendor \
     libcodec2_hidl@1.0.vendor \
+    libcodec2_soft_common.vendor \
     libcodec2_vndk.vendor \
+    libgui_vendor \
+    libstagefright_softomx.vendor \
     libstagefrighthw
 
 PRODUCT_PACKAGES += \
